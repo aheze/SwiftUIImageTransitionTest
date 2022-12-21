19716bb987ccbf99ee0182d3e6063f0d58f5ab3f
Zheng <aheze@getfind.app>
2022-12-21 07:53:25 -0800

    Base

--- SwiftUIImageTransitionTest/ContentView.swift
+++ SwiftUIImageTransitionTest/ContentView.swift
@@ -5,19 +5,49 @@
 //  Created by A. Zheng (github.com/aheze) on 12/21/22.
 //  Copyright © 2022 A. Zheng. All rights reserved.
 //
-    
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var index: Int = 1
+    @State private var forwards = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
+                .transition(
+                    .asymmetric(
+                        insertion: .move(edge: forwards ? .trailing : .leading),
+                        removal: .move(edge: forwards ? .leading : .trailing)
+                    )
+                )
+                .id(UUID())
+
+            Button("Next") {
+                index += 1
+                forwards = true
+                withAnimation {
+                    loadImage()
+                }
+            }
+
+            Button("Previous") {
+                index -= 1
+                forwards = false
+                withAnimation {
+                    loadImage()
+                }
+            }
         }
-        .padding()
+        .onAppear(perform: loadImage)
+    }
+
+    func loadImage() {
+        let name = String(format: "image%02d", index)
+        image = Image(name)
     }
 }
 
