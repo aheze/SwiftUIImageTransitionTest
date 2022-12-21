9c5c9f9ea02548b2f9a37305b0d90a29348b6fd8
Zheng <aheze@getfind.app>
2022-12-21 07:52:17 -0800

    Initial Commit

---
+++ SwiftUIImageTransitionTest/SwiftUIImageTransitionTestApp.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIImageTransitionTestApp.swift
+//  SwiftUIImageTransitionTest
+//
+//  Created by A. Zheng (github.com/aheze) on 12/21/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct SwiftUIImageTransitionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
